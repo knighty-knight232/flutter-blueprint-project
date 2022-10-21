b87a1dc016f7e4ce3950e453775a416e0a2b8358
danh_nguyen_232 <danhcathuy@gmail.com>
2022-10-21 22:34:07 +0700

    progress..

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,48 @@ void main() {
       MaterialApp(home: Scaffold(
         backgroundColor: Colors.deepPurple,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Center(
+              child: Text('Dicee'
+              ),
+          ),
           backgroundColor: Colors.deepPurple,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     )
   );
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: null,
+                child: Image.asset('images/dice1.png'
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
